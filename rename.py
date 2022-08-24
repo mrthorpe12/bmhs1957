6c6e44365ab8b06e666acc04da7422b1893eb5cc
mrthorpe <mrthorpe@umich.edu>
2022-08-24 15:20:22 -0400

    Rename files

--- rename.py
+++ rename.py
@@ -1,12 +1,12 @@
 import os
 
-imagedir = r"C:\Users\max\Documents\GitHub\bmhs1957\images\events\80thbirthday"
+imagedir = r"C:\Users\max\Documents\GitHub\bmhs1957\images\events\birthday80"
 imagenum = 1
 
 for image in os.listdir(imagedir):
+    # imagepath = os.path.join(imagedir, image)
     print(f'Image name is {image}')
     os.rename(os.path.join(imagedir, image),
-              os.path.join(imagedir, f'80thbirthday_{imagenum}.jpeg'))
-    # os.rename(image, f'80thbirthday_{imagenum}.jpeg')
+              os.path.join(imagedir, f'birthday80_{imagenum}.jpeg'))
     print(f'New name is {image}\n')
     imagenum += 1
